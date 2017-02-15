2bee0b8e6223d1b4106cb742f0c7e33a65e532a9
gajeelSF <SongYuda@lalala-4.local>
2017-02-14 21:26:08 -0800

    first commit

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,13 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by SongYuda on 2/14/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UICollectionViewCell {
+    
+}
