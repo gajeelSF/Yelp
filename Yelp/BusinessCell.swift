620f21c58090aeb742428021b921cc6db5603f9c
gajeelSF <SongYuda@lalala-4.local>
2017-02-14 21:51:11 -0800

    more functions

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -8,6 +8,37 @@
 
 import UIKit
 
-class BusinessCell: UICollectionViewCell {
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var reviewCountLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
     
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbImageView.setImageWith(business.imageURL!)
+            ratingImageView.setImageWith(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+            addressLabel.text = business.address
+            reviewCountLabel.text = "\(business.reviewCount)"
+            categoryLabel.text = business.categories
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
 }
